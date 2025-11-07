d9e6d1e499cc2e329efd022e050e0596d62f6c25
diyor0929 <diyorsultonov0727@gmai.com>
2025-11-07 22:53:47 +0500

    create django app

--- mydjangoapp/todo/tests.py
+++ mydjangoapp/todo/tests.py
@@ -6,6 +6,6 @@ from .models import Task
 
 class TaskModelTest(TestCase):
     def test_create_task(self):
-        """Ensure a Task can be created and saved."""
+        """Ensure a  Task can be created and saved."""
         task = Task.objects.create(title="Write CI/CD tests")
         self.assertEqual(task.title, "Write CI/CD tests")
